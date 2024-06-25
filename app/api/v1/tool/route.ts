d1cb3a65a71921c964a3cec297110c77db4e92f4
Jon Neal <jonneal3@gmail.com>
2024-06-25 14:13:14 -0400

    update tools

--- app/api/v1/tool/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server"
-
-export async function POST(req: NextRequest) {
-  return NextResponse.json({ message: "Hi" })
-}
-
-export async function OPTIONS() {
-  return NextResponse.json({ message: "Method not allowed" }, { status: 405 })
-}
